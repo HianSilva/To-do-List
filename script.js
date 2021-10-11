b5fc4e286758a86ca603774d1c80361557fe271c
Hian Silva <hiansilacts@gmail.com>
2021-10-11 10:01:05 -0300

    Revert "add undoTask and emptyAreaText features"
    This reverts commit bf00cc727db13080f537818ac3bf2feddd7cd372.

--- script.js
+++ script.js
@@ -1,39 +1,7 @@
-let taskNumber = 0
-
-function removeEmptyMessage() {
-  const toDoisEmpty = document.querySelector('#toDoArea').querySelectorAll('div').length === 0
-  const doneIsEmpty = document.querySelector('#doneArea').querySelectorAll('div').length === 0
-
-  const toDoText = document.querySelector('#emptyToDoArea')
-  const doneAreaText = document.querySelector('#emptyDoneArea')
-
-  if(!toDoisEmpty) {
-    toDoText.style.display = 'none'
-  }else {
-    toDoText.style.display = 'block'
-  }
-
-  if(!doneIsEmpty) {
-    doneAreaText.style.display = 'none'
-  }else {
-    doneAreaText.style.display = 'block'
-  }
-}
-
-function getTaskId() {
-  const test = document.querySelector('#task' + taskNumber)
-
-  if(test === null) {
-    return 'task' + taskNumber
-  }else{
-    return 'task' + (taskNumber+1)
-  }
-}
-
 function addTask() {
-  taskNumber++
+  const taskNumber = document.querySelector('#toDoArea').childElementCount
   const newTask = document.createElement('div');
-  const taskId = getTaskId();
+  const taskId = 'task' + taskNumber;
   
   newTask.setAttribute('class', 'task')
   newTask.setAttribute('id', taskId);
@@ -44,11 +12,11 @@ function addTask() {
   newTask.innerHTML = `
     <span>${text}</span>
     <div>
-      <button onClick='deleteTask(${taskId})')' id='deleteTaskButton'> 🗑 </button>
-      <button onClick='completeTask(${taskId})' id='completeTaskButton'> ✔ </button>
+      <button onClick='deleteTask(${taskId})' id='deleteButton'> ❌ </button>
+      <button onClick='completeTask(${taskId})' id='completeButton'> ✔ </button>
     </div>
   `
-  
+
   if(text == '') {
     alert('The task is empty')
   }else {
@@ -56,42 +24,18 @@ function addTask() {
     inputText.value = ""
     inputText.focus()
   }
-
-  removeEmptyMessage()
 }
 
+
 function deleteTask(value) {  
-  taskNumber--;
   const taskToDelete = document.querySelector('#' + value.id)
   const deleteArea = taskToDelete.parentElement.id
 
   document.querySelector('#' + deleteArea).removeChild(taskToDelete)
-
-  removeEmptyMessage()
 }
 
 function completeTask(value) {
   const taskCompleted = document.querySelector('#' + value.id)
-  const button = taskCompleted.querySelector('#completeTaskButton')
-  
-  button.setAttribute('onClick', `undoTask(${value.id})`)
-  button.setAttribute('id', 'undoTaskButton')
-  button.innerHTML = '✖'
 
   document.querySelector('#doneArea').appendChild(taskCompleted)
-
-  removeEmptyMessage()
-}
-
-function undoTask(value) {
-  const taskToUndo = document.querySelector('#' + value.id)
-  const button = taskToUndo.querySelector('#undoTaskButton')
-  
-  button.setAttribute('onClick', `completeTask(${value.id})`)
-  button.setAttribute('id', 'completeTaskButton')
-  button.innerHTML = '✔'
-
-  document.querySelector('#toDoArea').appendChild(taskToUndo)
-
-  removeEmptyMessage()
 }

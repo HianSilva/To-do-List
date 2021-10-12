21c57df03d8a19110e493ed24a9924c572b30540
Hian Silva <hiansilacts@gmail.com>
2021-10-12 13:20:17 -0300

    refactored code and addTaskWithEnter function

--- script.js
+++ script.js
@@ -1,41 +1,117 @@
-function addTask() {
-  const taskNumber = document.querySelector('#toDoArea').childElementCount
-  const newTask = document.createElement('div');
-  const taskId = 'task' + taskNumber;
-  
-  newTask.setAttribute('class', 'task')
-  newTask.setAttribute('id', taskId);
-  
-  const inputText = document.querySelector('#newTaskInput')
-  const text = inputText.value
-  
-  newTask.innerHTML = `
-    <span>${text}</span>
-    <div>
-      <button onClick='deleteTask(${taskId})' id='deleteButton'> ❌ </button>
-      <button onClick='completeTask(${taskId})' id='completeButton'> ✔ </button>
-    </div>
-  `
+let tasksNumber = 0
 
-  if(text == '') {
-    alert('The task is empty')
+const doneArea = document.querySelector('#doneArea')
+const toDoArea = document.querySelector('#toDoArea')
+const body = document.querySelector('body')
+
+body.addEventListener('keypress', (event) => { //Event to make possible add tasks with Enter Key
+  addTaskWithEnter(event)
+})
+
+function updateEmptyAreaAlert() {
+  const toDoIsEmpty = toDoArea.querySelectorAll('div').length === 0
+  const doneIsEmpty = doneArea.querySelectorAll('div').length === 0
+
+  const toDoAreaEmptyAlert = document.querySelector('#emptyToDoArea')
+  const doneAreaEmptyAlert = document.querySelector('#emptyDoneArea')
+
+  if(!toDoIsEmpty) {
+    toDoAreaEmptyAlert.style.display = 'none'
+    console.log('empty')
   }else {
-    document.querySelector('#toDoArea').appendChild(newTask)
-    inputText.value = ""
-    inputText.focus()
+    toDoAreaEmptyAlert.style.display = 'block'
+  }
+
+  if(!doneIsEmpty) {
+    doneAreaEmptyAlert.style.display = 'none'
+  }else {
+    doneAreaEmptyAlert.style.display = 'block'
   }
 }
 
+function getTaskId() {
+  const test = document.querySelector('#task' + tasksNumber)
 
-function deleteTask(value) {  
+  if(test === null) {
+    return 'task' + tasksNumber
+  }else{
+    return 'task' + (tasksNumber+1)
+  }
+}
+
+function addTaskWithEnter(event) {
+  const enterPressed = event.key == 'Enter'
+  const textInputIsOnFocus= document.querySelector('#newTaskInput') == document.activeElement
+  
+  if(enterPressed && textInputIsOnFocus) {
+    console.log('Enter Pressed')
+    addTask()
+  }
+}
+
+function addTask() {
+  tasksNumber++
+
+  const newTask = document.createElement('div')
+  const taskId = getTaskId()
+  const textInput = document.querySelector('#newTaskInput')
+  const taskText = textInput.value
+  
+  newTask.setAttribute('id', taskId)
+  newTask.setAttribute('class', 'task')
+
+  newTask.innerHTML = `
+    <span>${taskText}</span>
+    <div>
+      <button onClick='deleteTask(${taskId})')' id='deleteTaskButton'> 🗑 </button>
+      <button onClick='completeTask(${taskId})' id='completeTaskButton'> ✔ </button>
+    </div>
+  `
+
+  if(taskText == '') {
+    alert('The task have no content!')
+  }else {
+    toDoArea.appendChild(newTask)
+
+    textInput.value = ''
+    textInput.focus()
+  }
+
+  updateEmptyAreaAlert()
+}
+
+function deleteTask(value) {
+  tasksNumber--
   const taskToDelete = document.querySelector('#' + value.id)
-  const deleteArea = taskToDelete.parentElement.id
+  const taskAreaId = '#' + taskToDelete.parentElement.id //get the parent element from the task to be deleted
 
-  document.querySelector('#' + deleteArea).removeChild(taskToDelete)
+  document.querySelector(taskAreaId).removeChild(taskToDelete)
+
+  updateEmptyAreaAlert()
 }
 
 function completeTask(value) {
   const taskCompleted = document.querySelector('#' + value.id)
+  const button = taskCompleted.querySelector('#completeTaskButton')
+  
+  button.setAttribute('id', 'undoTaskButton')
+  button.setAttribute('onClick', `undoTask(${value.id})`)
+  button.innerHTML = '✖'
 
-  document.querySelector('#doneArea').appendChild(taskCompleted)
+  doneArea.appendChild(taskCompleted)
+
+  updateEmptyAreaAlert()
+}
+
+function undoTask(value) {
+  const taskToUndo = document.querySelector('#' + value.id)
+  const button = taskToUndo.querySelector('#undoTaskButton')
+  
+  button.setAttribute('onClick', `completeTask(${value.id})`)
+  button.setAttribute('id', 'completeTaskButton')
+  button.innerHTML = '✔'
+
+  toDoArea.appendChild(taskToUndo)
+
+  updateEmptyAreaAlert()
 }
